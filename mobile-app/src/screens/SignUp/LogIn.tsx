86dea538c12b473a7fb2c3d7c8f9377091c6577d
VictorBarros <victorbarros1130@gmail.com>
2021-05-18 22:29:47 -0300

    prettier

--- mobile-app/src/screens/SignUp/LogIn.tsx
+++ mobile-app/src/screens/SignUp/LogIn.tsx
@@ -1,21 +1,28 @@
 import React from 'react'
 import { StyleSheet, Text, View, Image } from 'react-native'
 import BottomTab from './BottomTab'
-import Svg, {Ellipse} from 'react-native-svg';
+import Svg, { Ellipse } from 'react-native-svg'
 
 const LogIn = () => {
   return (
     <View style={styles.container}>
       <View>
-        <View style={{height: 381, backgroundColor: '#3991E2', padding: 25, justifyContent: 'space-between'}}>
-          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-            <Text style={{fontWeight: 'bold', fontSize: 28}}>Training Buddy</Text>
+        <View
+          style={{
+            height: 381,
+            backgroundColor: '#3991E2',
+            padding: 25,
+            justifyContent: 'space-between',
+          }}
+        >
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Text style={{ fontWeight: 'bold', fontSize: 28 }}>Training Buddy</Text>
             <Image
-              style={{width: 110, height: 100, resizeMode: 'contain', borderRadius: 27}}
+              style={{ width: 110, height: 100, resizeMode: 'contain', borderRadius: 27 }}
               source={require('../../../assets/login_logo.png')}
             />
           </View>
-          <Text style={{fontFamily: 'monospace', fontSize: 13}}>
+          <Text style={{ fontFamily: 'monospace', fontSize: 13 }}>
             Agora que você já escolheu seu perfil, precisamos que crie um email e senha de acesso.
           </Text>
         </View>
@@ -24,9 +31,7 @@ const LogIn = () => {
         </Svg>
       </View>
 
-      <View>
-        {/* login with face, gmail or email */}
-      </View>
+      <View>{/* login with face, gmail or email */}</View>
 
       <BottomTab stage={2} />
     </View>
