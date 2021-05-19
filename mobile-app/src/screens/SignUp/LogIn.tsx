e79fdd663d5b52ff72782f7de0d0f2ec8016309c
VictorBarros <victorbarros1130@gmail.com>
2021-05-18 22:29:21 -0300

    login header

--- mobile-app/src/screens/SignUp/LogIn.tsx
+++ mobile-app/src/screens/SignUp/LogIn.tsx
@@ -1,23 +1,41 @@
-import { useNavigation } from '@react-navigation/native'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import BottomTab from './BottomTab'
+import Svg, {Ellipse} from 'react-native-svg';
 
 const LogIn = () => {
-  const navigation = useNavigation()
   return (
-    <>
-      <Text>login</Text>
+    <View style={styles.container}>
+      <View>
+        <View style={{height: 381, backgroundColor: '#3991E2', padding: 25, justifyContent: 'space-between'}}>
+          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+            <Text style={{fontWeight: 'bold', fontSize: 28}}>Training Buddy</Text>
+            <Image
+              style={{width: 110, height: 100, resizeMode: 'contain', borderRadius: 27}}
+              source={require('../../../assets/login_logo.png')}
+            />
+          </View>
+          <Text style={{fontFamily: 'monospace', fontSize: 13}}>
+            Agora que você já escolheu seu perfil, precisamos que crie um email e senha de acesso.
+          </Text>
+        </View>
+        <Svg height="65" width="100%">
+          <Ellipse cx="50%" rx="50%" ry="100%" fill="#3991E2" />
+        </Svg>
+      </View>
+
+      <View>
+        {/* login with face, gmail or email */}
+      </View>
 
       <BottomTab stage={2} />
-    </>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 30,
-    paddingTop: 40,
+    flex: 1,
     justifyContent: 'space-between',
   },
   text: {
