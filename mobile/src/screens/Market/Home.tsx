4f4926734b0a0e91c5d5569ad419bed7666fa741
VictorBarros <victorbarros1130@gmail.com>
2021-05-23 18:34:52 -0300

    add body to home screen

--- mobile/src/screens/Market/Home.tsx
+++ mobile/src/screens/Market/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, ViewProps, Image } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
 import Svg, { Ellipse } from 'react-native-svg'
 import Entypo from 'react-native-vector-icons/Entypo'
 import styles from './styles'
@@ -51,31 +52,72 @@ const Header = () => (
   </View>
 )
 
-const MarketHome = () => (
+const imageSource = (name: string) => {
+  switch (name.toLowerCase()) {
+    case 'surf': return require(`../../../assets/sport/surf.png`)
+    case 'canoagem': return require(`../../../assets/sport/canoagem.png`)
+    case 'raquete': return require(`../../../assets/rent/raquete.png`)
+  }
+}
+
+interface CardProps extends ViewProps {
+  name: string
+}
+
+const CardItem = ({ name }: CardProps) => {
+
+  return (
+  <TouchableOpacity style={{width: 80, height: 100, elevation: 5, marginVertical: 10, borderRadius: 20}} onPress={() => alert('not implemented yeat')}>
+    <Image source={imageSource(name)} style={{width: 80, height: 100, borderRadius: 20}}/>
+    <View style={{height: 20, width: 80, backgroundColor: '#D3D1D1', position: 'absolute', bottom: 0, borderRadius: 20, alignItems: 'center'}}>
+      <Text>{name}</Text>
+    </View>
+  </TouchableOpacity>
+)
+}
+
+const MarketHome = () => {
+  const toTrendingSports = ['Canoagem', 'Surf', 'Tênis']
+  const newestSports = ['Canoagem', 'Surf', 'Tênis']
+  const itemToRent = ['Prancha', 'Raquete', 'Roupa']
+
+  return (
   <View style={styles.root}>
     <Header />
 
-    <View style={{ paddingHorizontal: 25 }}>
+    <ScrollView style={{ paddingHorizontal: 25 }}>
       {/* body */}
       <View>
         <Text>Aulas mais procuradas</Text>
         <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-          {['Canoagem', 'Surf', 'Tênis'].map(sport => (
-            <Text key={`top-trending-${sport}`}>{sport}</Text>
+          {toTrendingSports.map(sport => (
+            <CardItem key={`top-trending-${sport}`} name={sport} />
           ))}
         </View>
       </View>
 
       <View>
         <Text>Recentes</Text>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          {newestSports.map(sport => (
+            <CardItem key={`top-trending-${sport}`} name={sport} />
+          ))}
+        </View>
       </View>
 
       <View>
         <Text>Aluguel de equipamentos e roupas</Text>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          {itemToRent.map(sport => (
+            <CardItem key={`top-trending-${sport}`} name={sport} />
+          ))}
+        </View>
       </View>
-    </View>
+    </ScrollView>
   </View>
 )
+}
+
 
 const extraStyles = StyleSheet.create({
   topContainer: {
